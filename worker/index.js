321e489294d93e9172e30b58aa839fa5e3924753
mark-mdev <markmorgan.dev@gmail.com>
2025-08-05 17:34:34 -0700

    updated version of packages

--- worker/index.js
+++ worker/index.js
@@ -1,11 +1,10 @@
-const keys = require('./keys');
-const redis = require('redis');
+const keys = require("./keys");
+const redis = require("redis");
 
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000,
+  url: `redis://${keys.redisHost}:${keys.redisPort}`,
 });
+
 const sub = redisClient.duplicate();
 
 function fib(index) {
@@ -13,7 +12,11 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fib(parseInt(message)));
-});
-sub.subscribe('insert');
+(async () => {
+  await redisClient.connect();
+  await sub.connect();
+
+  sub.subscribe("insert", (message) => {
+    redisClient.hSet("values", message, fib(parseInt(message)));
+  });
+})();
