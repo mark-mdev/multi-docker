a1378b54373c2fb07f6d9918bcbdaf1638b9a9aa
mark-mdev <markmorgan.dev@gmail.com>
2025-08-09 00:16:09 -0700

    redeploy

--- server/index.js
+++ server/index.js
@@ -17,9 +17,12 @@ const pgClient = new Pool({
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.NODE_ENV !== "production"
+      ? false
+      : {
+          rejectUnauthorized: false,
+        },
 });
 
 pgClient.on("connect", (client) => {
